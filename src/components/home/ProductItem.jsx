4eaad3efea1cc08c7f78793d2f354420e6ee90db
Aljith K J <kwdeathlok@gmail.com>
2025-04-09 10:45:01 +0530

    Add ProductItem component

---
+++ src/components/home/ProductItem.jsx
@@ -0,0 +1,17 @@
+import Heart from '../../assets/heart.svg'
+import Image from 'next/image'
+
+export const ProductItem = ({ image, title, description }) => {
+    return (
+        <div className='product-item'>
+            <Image src={image} alt="product image" width={100} height={100} className='product-image'/>
+            <div>
+                <p>{ title.length > 30 ? title.slice(0, 30) + "..." : title }</p>
+                <div>
+                    <span>{ description.length > 40 ? description.slice(0, 40) + "..." : description }</span>
+                    <Image src={Heart} alt="Heart" width={25} />
+                </div>
+            </div>
+        </div>
+    )
+}
